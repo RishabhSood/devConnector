dcb86ea275f81e217e4124a4382388469871a52d
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-14 10:47:59 +0530

    Added routes folder with individual js files, defined routes in server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/API/users'));
+app.use('/api/auth', require('./routes/API/auth'));
+app.use('/api/profile', require('./routes/API/profile'));
+app.use('/api/posts', require('./routes/API/posts'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
