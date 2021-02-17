616083d33b287fc1938840bd37ffca302e3867ab
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-17 15:02:09 +0530

    Created Redux Store and Setup Alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'; // connect react component to redux store, has to be exported at end
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
 
-function Register() {
+function Register({ setAlert }) {
 
     const [formData, setformData] = useState({
         name: '',
@@ -18,7 +21,7 @@ function Register() {
     const onSubmit = e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('passwords do not match');
+            setAlert('passwords do not match', 'danger');
         } else {
             console.log(formData);
         }
@@ -86,4 +89,7 @@ function Register() {
     )
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+}
+export default connect(null, { setAlert })(Register)
