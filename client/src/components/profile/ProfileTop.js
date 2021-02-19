ba0ff5a41bed84612e617e0a3523aad073017fbb
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-19 18:08:35 +0530

    final commit

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -27,25 +27,25 @@ const ProfileTop = ({ profile: { status, company, location, website, social, use
                     )}
 
                     {social && social.facebook && (
-                        <a href="#" target="_blank" rel="noopener noreferrer">
+                        <a href={social.facebook} target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-facebook fa-2x"></i>
                         </a>
                     )}
 
                     {social && social.linkedin && (
-                        <a href="#" target="_blank" rel="noopener noreferrer">
+                        <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-linkedin fa-2x"></i>
                         </a>
                     )}
 
                     {social && social.youtube && (
-                        <a href="#" target="_blank" rel="noopener noreferrer">
+                        <a href={social.youtube} target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-youtube fa-2x"></i>
                         </a>
                     )}
 
                     {social && social.instagram && (
-                        <a href="#" target="_blank" rel="noopener noreferrer">
+                        <a href={social.instagram} target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-instagram fa-2x"></i>
                         </a>
                     )}
