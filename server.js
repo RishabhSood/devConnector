89cda73c6217c3778ec66bbc95c594debb13fbb3
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-19 13:04:04 +0530

    production ready

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-//Init Middleware - to get data in request.body
-app.use(express.json({ extended: false }));
+// Init Middleware
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-// Define routes
-app.use('/api/users', require('./routes/API/users'));
-app.use('/api/auth', require('./routes/API/auth'));
-app.use('/api/profile', require('./routes/API/profile'));
-app.use('/api/posts', require('./routes/API/posts'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
