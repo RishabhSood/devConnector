30360a2dd18b3638951d57a8cebb80534a6ffc51
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-19 12:10:13 +0530

    changes

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
