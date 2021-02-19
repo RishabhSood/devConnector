b07ccaed3b9dbeed803ffe67baec8726c18af63e
Rishabh Sood <sood.rishabh@hotmail.com>
2021-02-19 12:52:52 +0530

    completed build

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use('/api/auth', require('./routes/API/auth'));
 app.use('/api/profile', require('./routes/API/profile'));
 app.use('/api/posts', require('./routes/API/posts'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
